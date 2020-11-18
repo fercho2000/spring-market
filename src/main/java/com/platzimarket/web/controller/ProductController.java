440e89ebb952fe131981965d92c3e12f5638ba1a
joseusuga <jose.usuga@ceiba.com.co>
2020-11-18 09:16:26 -0500

    GRAL: Dependencias y documentar api con swagger

--- src/main/java/com/platzimarket/web/controller/ProductController.java
+++ src/main/java/com/platzimarket/web/controller/ProductController.java
@@ -2,6 +2,10 @@ package com.platzimarket.web.controller;
 
 import com.platzimarket.domain.Product;
 import com.platzimarket.domain.service.ProductServices;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,13 +21,22 @@ public class ProductController {
     private ProductServices productServices;
 
     @GetMapping("/all")
+    @ApiOperation("Get all supermarket products")
+    @ApiResponse(code = 200, message = "OK")
     public ResponseEntity<List<Product>> getAll() {
 
         return new ResponseEntity<>(productServices.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Product> getProduct(@PathVariable("id") int productId) {
+    @ApiOperation("Search a product with an ID")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "Product not found")
+    })
+    public ResponseEntity<Product> getProduct(
+            @ApiParam(value = "The id of the product", required = true, example = "7")
+            @PathVariable("id") int productId) {
         return productServices.getProduct(productId)
                 .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
@@ -48,7 +61,7 @@ public class ProductController {
         if (productServices.delete(productId))
             return new ResponseEntity<>(HttpStatus.OK);
         else
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 
