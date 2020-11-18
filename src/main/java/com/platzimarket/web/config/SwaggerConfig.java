e534842df9624585c661fe5735fa0761de3dfc69
joseusuga <jose.usuga@ceiba.com.co>
2020-11-18 15:45:17 -0500

    GRAL: Dependencias y documentar api con swagger

--- src/main/java/com/platzimarket/web/config/SwaggerConfig.java
+++ src/main/java/com/platzimarket/web/config/SwaggerConfig.java
@@ -2,14 +2,23 @@ package com.platzimarket.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Api de productos")
+                .description("Sservicios de consulta y guardado")
+                .license("Apache 2.0")
+                .version("1.0.0")
+                .build();
+    }
+
 
     @Bean
     public Docket api() {
@@ -19,4 +28,5 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.platzimarket.web.controller"))
                 .build();
     }
+
 }
